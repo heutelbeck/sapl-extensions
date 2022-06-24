6c00cb35cd3bb80f5d5d38df58cf97180c0ef180
Dominik Wuttke <Dominikwuttke@web.de>
2022-06-24 13:54:27 +0200

    - add additional ressources to AuthorizationSubscriptionBuilderService

--- sapl-axon/sapl-axon-backend/src/main/java/io/sapl/axon/utilities/AuthorizationSubscriptionBuilderService.java
+++ sapl-axon/sapl-axon-backend/src/main/java/io/sapl/axon/utilities/AuthorizationSubscriptionBuilderService.java
@@ -16,14 +16,16 @@
 
 package io.sapl.axon.utilities;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Executable;
-import java.util.Map;
-import java.util.Optional;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.sapl.api.pdp.AuthorizationSubscription;
+import io.sapl.spring.method.metadata.PreEnforce;
+import lombok.RequiredArgsConstructor;
 import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.messaging.Message;
 import org.axonframework.messaging.annotation.MessageHandlingMember;
+import org.axonframework.messaging.responsetypes.ResponseType;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 import org.axonframework.queryhandling.QueryMessage;
 import org.axonframework.spring.stereotype.Aggregate;
@@ -35,13 +37,10 @@ import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import io.sapl.api.pdp.AuthorizationSubscription;
-import io.sapl.spring.method.metadata.PreEnforce;
-import lombok.RequiredArgsConstructor;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Executable;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * The AuthorizationSubscriptionBuilderService Object offers methods to get the
@@ -52,206 +51,217 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthorizationSubscriptionBuilderService {
 
-	private static final String ENVIRONMENT = "environment";
-	private static final String RESOURCE    = "resource";
-	private static final String ACTION      = "action";
-	private static final String SUBJECT     = "subject";
-	private final ObjectMapper  mapper;
+    private static final String ENVIRONMENT = "environment";
+    private static final String RESOURCE = "resource";
+    private static final String ACTION = "action";
+    private static final String SUBJECT = "subject";
+    private final ObjectMapper mapper;
 
-	/**
-	 * Executed to get the AuthorizationSubscription for a Query Message. It gets
-	 * the queryMessage and the annotation and returns the constructed
-	 * AuthorizationSubscription.
-	 * 
-	 * @param queryMessage Representation of a QueryMessage, containing a Payload
-	 *                     and MetaData
-	 * @param annotation   Annotation from the respective method
-	 * @param executable   an execuatable
-	 * @param queryResult  a query result
-	 * @return the AuthorizationSubscription for the Query
-	 */
-	public AuthorizationSubscription constructAuthorizationSubscriptionForQuery(
-			QueryMessage<?, ?> queryMessage,
-			Annotation annotation,
-			Executable executable,
-			Optional<?> queryResult) {
+    /**
+     * Executed to get the AuthorizationSubscription for a Query Message. It gets
+     * the queryMessage and the annotation and returns the constructed
+     * AuthorizationSubscription.
+     *
+     * @param queryMessage Representation of a QueryMessage, containing a Payload
+     *                     and MetaData
+     * @param annotation   Annotation from the respective method
+     * @param executable   an execuatable
+     * @param queryResult  a query result
+     * @return the AuthorizationSubscription for the Query
+     */
+    public AuthorizationSubscription constructAuthorizationSubscriptionForQuery(
+            QueryMessage<?, ?> queryMessage,
+            Annotation annotation,
+            Executable executable,
+            Optional<?> queryResult) {
 
-		var annotationAttributeValues = retrieveAttributeValuesFromAnnotation(annotation);
+        var annotationAttributeValues = retrieveAttributeValuesFromAnnotation(annotation);
 
-		var subject     = retrieveSubject(queryMessage, annotationAttributeValues);
-		var action      = retrieveAction(queryMessage, annotationAttributeValues);
-		var resource    = retrieveResourceFromQueryMessage(queryMessage, annotationAttributeValues, executable,
-				queryResult);
-		var environment = retrieveEnvironment(annotationAttributeValues);
+        var subject = retrieveSubject(queryMessage, annotationAttributeValues);
+        var action = retrieveAction(queryMessage, annotationAttributeValues);
+        var resource = retrieveResourceFromQueryMessage(queryMessage, annotationAttributeValues, executable,
+                queryResult);
+        var environment = retrieveEnvironment(annotationAttributeValues);
 
-		return AuthorizationSubscription.of(subject, action, resource, environment);
-	}
+        return AuthorizationSubscription.of(subject, action, resource, environment);
+    }
 
-	/**
-	 * Executed to get the AuthorizationSubscription for a Command Message. It gets
-	 * the message, target and the delegate and returns the constructed
-	 * AuthorizationSubscription.
-	 * 
-	 * @param message   Representation of a Message, containing a Payload and
-	 *                  MetaData
-	 * @param aggregate Object, which contains state and methods to alter that state
-	 * @param delegate  Command handler, needed to set parameter from annotation
-	 * @return the AuthorizationSubscription for the Command
-	 */
+    /**
+     * Executed to get the AuthorizationSubscription for a Command Message. It gets
+     * the message, target and the delegate and returns the constructed
+     * AuthorizationSubscription.
+     *
+     * @param message   Representation of a Message, containing a Payload and
+     *                  MetaData
+     * @param aggregate Object, which contains state and methods to alter that state
+     * @param delegate  Command handler, needed to set parameter from annotation
+     * @return the AuthorizationSubscription for the Command
+     */
 
-	public AuthorizationSubscription constructAuthorizationSubscriptionForCommand(
-			CommandMessage<?> message,
-			Object aggregate,
-			MessageHandlingMember<?> delegate) {
+    public AuthorizationSubscription constructAuthorizationSubscriptionForCommand(
+            CommandMessage<?> message,
+            Object aggregate,
+            MessageHandlingMember<?> delegate) {
 
-		Annotation methodAnnotation          = retrievePreEnforceAnnotation(delegate);
-		var        annotationAttributeValues = retrieveAttributeValuesFromAnnotation(methodAnnotation);
+        Annotation methodAnnotation = retrievePreEnforceAnnotation(delegate);
+        var annotationAttributeValues = retrieveAttributeValuesFromAnnotation(methodAnnotation);
 
-		var subject     = retrieveSubject(message, annotationAttributeValues);
-		var action      = retrieveAction(message, annotationAttributeValues);
-		var resource    = retrieveResourceFromTarget(message, aggregate, annotationAttributeValues);
-		var environment = retrieveEnvironment(annotationAttributeValues);
+        var subject = retrieveSubject(message, annotationAttributeValues);
+        var action = retrieveAction(message, annotationAttributeValues);
+        var resource = retrieveResourceFromTarget(message, aggregate, annotationAttributeValues);
+        var environment = retrieveEnvironment(annotationAttributeValues);
 
-		return AuthorizationSubscription.of(subject, action, resource, environment);
-	}
+        return AuthorizationSubscription.of(subject, action, resource, environment);
+    }
 
-	private Map<String, Object> retrieveAttributeValuesFromAnnotation(Annotation annotation) {
-		return AnnotationUtils.getAnnotationAttributes(annotation);
-	}
+    private Map<String, Object> retrieveAttributeValuesFromAnnotation(Annotation annotation) {
+        return AnnotationUtils.getAnnotationAttributes(annotation);
+    }
 
-	private JsonNode retrieveSubject(Message<?> message, Map<String, Object> values) {
-		var subjectNode = mapper.createObjectNode();
-		var subject     = values.get(SUBJECT).toString();
+    private JsonNode retrieveSubject(Message<?> message, Map<String, Object> values) {
+        var subjectNode = mapper.createObjectNode();
+        var subject = values.get(SUBJECT).toString();
 
-		if (!subject.isBlank()) {
-			var expr = setUpExpression(subject).getValue();
-			if (expr != null)
-				return subjectNode.put("name", expr.toString());
-		}
+        if (!subject.isBlank()) {
+            var expr = setUpExpression(subject).getValue();
+            if (expr != null)
+                return subjectNode.put("name", expr.toString());
+        }
 
-		if (message.getMetaData().get(SUBJECT) == null)
-			return null;
+        if (message.getMetaData().get(SUBJECT) == null)
+            return null;
 
-		return subjectNode.setAll((ObjectNode) message.getMetaData().get(SUBJECT));
-	}
+        return subjectNode.setAll((ObjectNode) message.getMetaData().get(SUBJECT));
+    }
 
-	private JsonNode retrieveAction(Message<?> message, Map<String, Object> values) {
-		var actionAnnotation = values.get(ACTION).toString();
-		var actionNode       = mapper.createObjectNode();
+    private JsonNode retrieveAction(Message<?> message, Map<String, Object> values) {
+        var actionAnnotation = values.get(ACTION).toString();
+        var actionNode = mapper.createObjectNode();
 
-		if (!actionAnnotation.isBlank())
-			actionNode.set(actionAnnotation, evaluateSpEL(message.getPayload(), actionAnnotation));
+        if (!actionAnnotation.isBlank())
+            actionNode.set(actionAnnotation, evaluateSpEL(message.getPayload(), actionAnnotation));
 
-		constructActionWithMessage(message, actionNode);
-		return actionNode;
-	}
+        constructActionWithMessage(message, actionNode);
+        return actionNode;
+    }
 
-	private void constructActionWithMessage(Message<?> message, ObjectNode actionNode) {
-		actionNode.put("name", message.getPayloadType().getSimpleName());
-		actionNode.set("message", mapper.valueToTree(message.getPayload()));
-		actionNode.set("metadata", mapper.valueToTree(message.getMetaData()));
-	}
+    private void constructActionWithMessage(Message<?> message, ObjectNode actionNode) {
+        actionNode.put("name", message.getPayloadType().getSimpleName());
+        actionNode.set("message", mapper.valueToTree(message.getPayload()));
+        actionNode.set("metadata", mapper.valueToTree(message.getMetaData()));
+    }
 
-	private JsonNode retrieveResourceFromQueryMessage(
-			QueryMessage<?, ?> message,
-			Map<String, Object> values,
-			Executable executable,
-			Optional<?> queryResult) {
-		var resourceNode       = mapper.createObjectNode();
-		var resourceAnnotation = values.get(RESOURCE).toString();
+    private JsonNode retrieveResourceFromQueryMessage(
+            QueryMessage<?, ?> message,
+            Map<String, Object> values,
+            Executable executable,
+            Optional<?> queryResult) {
+        var resourceNode = mapper.createObjectNode();
+        var resourceAnnotation = values.get(RESOURCE).toString();
 
-		if (!resourceAnnotation.isBlank()) {
-			var expr = setUpExpression(resourceAnnotation).getValue();
-			if (expr != null)
-				resourceNode.put(RESOURCE, expr.toString());
-		}
+        if (!resourceAnnotation.isBlank()) {
+            var expr = setUpExpression(resourceAnnotation).getValue();
+            if (expr != null)
+                resourceNode.put(RESOURCE, expr.toString());
+        }
 
-		queryResult.ifPresent(result -> resourceNode.set("queryResult", mapper.valueToTree(result)));
+        queryResult.ifPresent(result -> resourceNode.set("queryResult", mapper.valueToTree(result)));
 
-		resourceNode.put("projectionClass", executable.getDeclaringClass().getSimpleName());
-		resourceNode.put("methodName", executable.getName());
-		resourceNode.put("responsetype", message.getResponseType().getExpectedResponseType().getSimpleName());
-		resourceNode.put("queryname", message.getPayloadType().getSimpleName());
+        var responseType = message.getResponseType().getExpectedResponseType().getSimpleName();
 
-		if (message.getPayloadType().getEnclosingClass() != null)
-			resourceNode.put("classname", message.getPayloadType().getEnclosingClass().getSimpleName());
+        resourceNode.put("projectionClass", executable.getDeclaringClass().getSimpleName());
+        resourceNode.put("methodName", executable.getName());
+        resourceNode.put("responseType", responseType);
+        resourceNode.put("queryName", message.getPayloadType().getSimpleName());
 
-		return resourceNode;
-	}
+        var isSubscriptionQuery = message.getMetaData().containsKey("updateResponseType");
 
-	private Object retrieveEnvironment(Map<String, Object> values) {
-		var env = values.get(ENVIRONMENT).toString();
-		if (env.isBlank())
-			return null;
+        if (isSubscriptionQuery){
+            var updateResponseType = ((ResponseType<?>) message.getMetaData().get("updateResponseType")).getExpectedResponseType().getSimpleName();
+            resourceNode.put("updateResponseType", updateResponseType);
+            resourceNode.put("sameResponseTypes", responseType.equals(updateResponseType));
+        }
+        resourceNode.put("isSubscriptionQuery",isSubscriptionQuery);
 
-		var expr = setUpExpression(env).getValue();
-		if (expr == null)
-			return null;
+        if (message.getPayloadType().getEnclosingClass() != null)
+            resourceNode.put("classname", message.getPayloadType().getEnclosingClass().getSimpleName());
 
-		return expr.toString();
+        return resourceNode;
+    }
 
-	}
+    private Object retrieveEnvironment(Map<String, Object> values) {
+        var env = values.get(ENVIRONMENT).toString();
+        if (env.isBlank())
+            return null;
 
-	private <T> Annotation retrievePreEnforceAnnotation(MessageHandlingMember<T> handler) {
-		var method = handler.unwrap(Executable.class).orElseThrow();
-		return AnnotationUtils.getAnnotation(method, PreEnforce.class);
-	}
+        var expr = setUpExpression(env).getValue();
+        if (expr == null)
+            return null;
 
-	private <T> JsonNode retrieveResourceFromTarget(Message<?> message, T aggregate, Map<String, Object> values) {
-		var resourceNode       = mapper.createObjectNode();
-		var resourceAnnotation = values.get(RESOURCE).toString();
+        return expr.toString();
 
-		if (!resourceAnnotation.isBlank())
-			resourceNode.set(resourceAnnotation, evaluateSpEL(aggregate, resourceAnnotation));
+    }
 
-		constructResourceWithAggregateInformation(message, aggregate, resourceNode);
+    private <T> Annotation retrievePreEnforceAnnotation(MessageHandlingMember<T> handler) {
+        var method = handler.unwrap(Executable.class).orElseThrow();
+        return AnnotationUtils.getAnnotation(method, PreEnforce.class);
+    }
 
-		return resourceNode;
-	}
+    private <T> JsonNode retrieveResourceFromTarget(Message<?> message, T aggregate, Map<String, Object> values) {
+        var resourceNode = mapper.createObjectNode();
+        var resourceAnnotation = values.get(RESOURCE).toString();
 
-	private <T> void constructResourceWithAggregateInformation(
-			Message<?> message,
-			T aggregate,
-			ObjectNode resourceNode) {
+        if (!resourceAnnotation.isBlank())
+            resourceNode.set(resourceAnnotation, evaluateSpEL(aggregate, resourceAnnotation));
 
-		var aggregateType = message.getMetaData().get(Constants.aggregateType.name());
-		if (aggregateType != null)
-			resourceNode.set(Constants.aggregateType.name(), mapper.valueToTree(aggregateType));
-		else if (aggregate != null)
-			if (aggregate.getClass().isAnnotationPresent(Aggregate.class))
-				resourceNode.put(Constants.aggregateType.name(), aggregate.getClass().getSimpleName());
+        constructResourceWithAggregateInformation(message, aggregate, resourceNode);
 
-		var fields = message.getPayloadType().getDeclaredFields();
-		for (var field : fields) {
-			if (field.isAnnotationPresent(TargetAggregateIdentifier.class)) {
-				resourceNode.set(Constants.aggregateIdentifier.name(),
-						evaluateSpEL(message.getPayload(), field.getName()));
-				break;
-			}
-		}
-	}
+        return resourceNode;
+    }
 
-	private JsonNode evaluateToJson(Expression expr, StandardEvaluationContext ctx) {
-		try {
-			return mapper.valueToTree(expr.getValue(ctx));
-		} catch (EvaluationException e) {
-			throw new IllegalArgumentException("Failed to evaluate expression '" + expr.getExpressionString() + "'", e);
-		}
-	}
+    private <T> void constructResourceWithAggregateInformation(
+            Message<?> message,
+            T aggregate,
+            ObjectNode resourceNode) {
 
-	private StandardEvaluationContext setUpEvaluationContext(Object object) {
-		return new StandardEvaluationContext(object);
-	}
+        var aggregateType = message.getMetaData().get(Constants.aggregateType.name());
+        if (aggregateType != null)
+            resourceNode.set(Constants.aggregateType.name(), mapper.valueToTree(aggregateType));
+        else if (aggregate != null)
+            if (aggregate.getClass().isAnnotationPresent(Aggregate.class))
+                resourceNode.put(Constants.aggregateType.name(), aggregate.getClass().getSimpleName());
 
-	private Expression setUpExpression(String expr) {
-		ExpressionParser expressionParser = new SpelExpressionParser();
-		return expressionParser.parseExpression(expr);
-	}
+        var fields = message.getPayloadType().getDeclaredFields();
+        for (var field : fields) {
+            if (field.isAnnotationPresent(TargetAggregateIdentifier.class)) {
+                resourceNode.set(Constants.aggregateIdentifier.name(),
+                        evaluateSpEL(message.getPayload(), field.getName()));
+                break;
+            }
+        }
+    }
 
-	private JsonNode evaluateSpEL(Object obj, String expr) {
-		var context    = setUpEvaluationContext(obj);
-		var expression = setUpExpression(expr);
-		return evaluateToJson(expression, context);
+    private JsonNode evaluateToJson(Expression expr, StandardEvaluationContext ctx) {
+        try {
+            return mapper.valueToTree(expr.getValue(ctx));
+        } catch (EvaluationException e) {
+            throw new IllegalArgumentException("Failed to evaluate expression '" + expr.getExpressionString() + "'", e);
+        }
+    }
 
-	}
+    private StandardEvaluationContext setUpEvaluationContext(Object object) {
+        return new StandardEvaluationContext(object);
+    }
+
+    private Expression setUpExpression(String expr) {
+        ExpressionParser expressionParser = new SpelExpressionParser();
+        return expressionParser.parseExpression(expr);
+    }
+
+    private JsonNode evaluateSpEL(Object obj, String expr) {
+        var context = setUpEvaluationContext(obj);
+        var expression = setUpExpression(expr);
+        return evaluateToJson(expression, context);
+
+    }
 }
