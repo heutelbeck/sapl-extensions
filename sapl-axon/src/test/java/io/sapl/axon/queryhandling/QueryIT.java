43b24c1a9351bb73bc73b8cfbfeb2abe805449b0
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2023-11-09 15:19:18 +0100

    update to new AxonContainer

--- sapl-axon/src/test/java/io/sapl/axon/queryhandling/QueryIT.java
+++ sapl-axon/src/test/java/io/sapl/axon/queryhandling/QueryIT.java
@@ -17,15 +17,12 @@
  */
 package io.sapl.axon.queryhandling;
 
-import java.time.Duration;
-
+import org.axonframework.test.server.AxonServerContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
@@ -35,17 +32,14 @@ import org.testcontainers.utility.DockerImageName;
 public class QueryIT extends QueryTestsuite {
     private static final int AXON_SERVER_GRPC_PORT = 8124;
 
-    private static final long TIMEOUT_FOR_AXON_SERVER_SPINUP = 40L;
-
     @Container
-    static GenericContainer<?> axonServer = new GenericContainer<>(DockerImageName.parse("axoniq/axonserver"))
-            .withExposedPorts(8024, 8124).waitingFor(Wait.forHttp("/actuator/info").forPort(8024))
-            .withStartupTimeout(Duration.ofSeconds(TIMEOUT_FOR_AXON_SERVER_SPINUP));
+    static final AxonServerContainer AXON_SERVER = new AxonServerContainer(
+            DockerImageName.parse("axoniq/axonserver:latest-dev"));
 
     @DynamicPropertySource
     static void registerAxonProperties(DynamicPropertyRegistry registry) {
         registry.add("axon.axonserver.servers",
-                () -> axonServer.getHost() + ":" + axonServer.getMappedPort(AXON_SERVER_GRPC_PORT));
+                () -> AXON_SERVER.getHost() + ":" + AXON_SERVER.getMappedPort(AXON_SERVER_GRPC_PORT));
     }
 
     @SpringBootApplication
